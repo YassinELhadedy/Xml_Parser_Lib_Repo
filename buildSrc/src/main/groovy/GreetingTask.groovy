cd6808a617a712ae3238adf2f69a0db267ff1b6d
yassinelhadedy <yassinelhadedy@hotmail.com>
2021-04-27 02:23:30 +0200

    revert changes

--- buildSrc/src/main/groovy/GreetingTask.groovy
+++
@@ -1,9 +0,0 @@
-import org.gradle.api.tasks.TaskAction
-import org.gradle.api.DefaultTask
-
-class GreetingTask extends DefaultTask {
-    @TaskAction
-    def greet() {
-        println 'hello from GreetingTask'
-    }
-}
